d14575f4ad210cd8c155cb396038fefa1c549af6
blayne-04 <befuller2004@gmail.com>
2023-04-04 13:24:51 -0700

    adding signup functionality, signup submit not working

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,11 +1,42 @@
-const signUp = async() => {
-    const userName = document.querySelector('#userName').value.trim().toLowerCase()
-    const email = document.querySelector('#suEmail').value.trim()
-    const password =  document.querySelector('suPassword').value.trim()
+const signUp = async (event) => {
+    event.preventDefault()
+    
+  const userName = document.querySelector("#userName").value.trim().toLowerCase();
+  const email = document.querySelector("#suEmail").value.trim();
+  const password = document.querySelector("#suPassword").value.trim();
+  const password2 = document.querySelector("#suPassword2").value.trim();
 
-    const response = await fetch('/api/users/signup', {
-        method: 'POST',
-        body: JSON.stringify({email, password}),
-        headers: {'Content-Type': 'application/json'}
-    })
+  switch(false){
+    case(password === password2):
+        console.log('Passwords Don\'nt match')
+        break;
+    case validPW(password):
+        console.log('Invalid Password must contain A-Z a-z 1 !')
+        break;
+    case validEM(email):
+        console.log('Invalid Email Address')
+        break;
+    case validUN(userName): 
+        console.log('Username must be at least 4 characters long and alphanumeric')
+        break;
+    default: 
+    const response = await fetch("/api/users/signup", {
+        method: "POST",
+        body: JSON.stringify({ userName, email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+  }
 }
+document.querySelector(".signup-form").addEventListener("submit", signUp);
+
+const validUN = (UN) => {
+  return /^[a-zA-Z0-9]{4,}$/.test(UN);
+};
+
+const validEM = (EM) => {
+  return /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/.test(EM);
+};
+
+const validPW = (PW) => {
+  return /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,64})/.test(PW);
+};
